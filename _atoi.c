2af85cc72ae11c686a3671df98dbe68569aedeff
Augustine Edwin <96816882+auztintech@users.noreply.github.com>
2022-11-16 22:00:05 +0100

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interacti ve - returns true if shell is interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
